9d6c8e4b55bca6eac72b5393400fe3ce8750bfe2
Leonardo Rico <duvan.rico311@pascualbravo.edu.co>
2024-08-24 19:03:23 -0500

    fix(calendar): resolve issue with calendar screen

--- app/src/main/java/leo/rico/primeraentrega/MainActivity.java
+++ app/src/main/java/leo/rico/primeraentrega/MainActivity.java
@@ -1,5 +1,6 @@
 package leo.rico.primeraentrega;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
@@ -12,15 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Calendar;
 import android.app.DatePickerDialog;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private ImageButton btnOpenCalendar;
     private TextView txtdateImput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate Called");
+        // Configuración inicial de la actividad
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -29,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        //Inicializar componentes
         btnOpenCalendar = findViewById(R.id.btnopen_calendar);
         txtdateImput = findViewById(R.id.txtdateInput);
 
+        //Configurar listeners
         btnOpenCalendar.setOnClickListener(v -> showDatePickerDialog());
     }
 
@@ -39,26 +46,79 @@ public class MainActivity extends AppCompatActivity {
         // Obtener la fecha actual
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
+        int MONTH = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         // Crear el DatePickerDialog
         DatePickerDialog datePickerDialog = new DatePickerDialog(
                 this,
                 new DatePickerDialog.OnDateSetListener() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         // Formatear y mostrar la fecha seleccionada
-                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
-                        txtdateImput.setText(selectedDate);
+                        String formattedDay, formattedMonth;
+
+                        //Obtener dia
+                        if (dayOfMonth < 10){
+                            formattedDay = 0 + String.valueOf(dayOfMonth);
+                        }else{
+                            formattedDay = String.valueOf(dayOfMonth);
+                        }
+
+                        //Obtener mes
+                        int MONTH = month + 1;
+                        if (MONTH < 10){
+                            formattedMonth = 0 + String.valueOf(month);
+                        }else{
+                            formattedMonth = String.valueOf(month);
+                        }
+
+                        //settear fecha en textView
+                        txtdateImput.setText(formattedDay + "/" + formattedMonth + "/" + year);
                     }
                 },
                 year,
-                month,
+                MONTH,
                 day
         );
 
         // Mostrar el DatePickerDialog
         datePickerDialog.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+        //iniciar o reanudar procesos que deben estar activos mientras la actividad está visible
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+        // Aquí puedes reiniciar procesos que deben estar activos mientras la actividad está en primer plano
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+        // Aquí puedes guardar el estado o detener procesos que no deben continuar mientras la actividad está en segundo plano
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+        // Aquí puedes liberar recursos que no se necesitan mientras la actividad no está en primer plano
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+        // Aquí puedes limpiar recursos o liberar memoria
     }
 }
